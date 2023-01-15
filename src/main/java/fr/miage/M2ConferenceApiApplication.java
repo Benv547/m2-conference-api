83ebaeadc68d6723770140c6d95d8b8da80f4606
Benjamin VIGNOT <benji.vignot@gmail.com>
2023-01-15 14:25:13 +0100

    First commit (init app with starter)

---
+++ src/main/java/fr/miage/M2ConferenceApiApplication.java
@@ -0,0 +1,13 @@
+package fr.miage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2ConferenceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M2ConferenceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/miage/M2ConferenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2ConferenceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
