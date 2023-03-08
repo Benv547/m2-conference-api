c5ee7d9842ef21dccf161451c4773dbff413171f
Benjamin VIGNOT <benji.vignot@gmail.com>
2023-03-08 15:12:12 +0100

    feat: CI -> maven tests avant merge request (#10)

--- src/test/java/fr/miage/M2ConferenceApiApplicationTests.java
+++ src/test/java/fr/miage/M2ConferenceApiApplicationTests.java
@@ -2,12 +2,15 @@ package fr.miage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest
 class M2ConferenceApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    void isTrue() {
+        assertThat(true, equalTo(true));
     }
 
 }
