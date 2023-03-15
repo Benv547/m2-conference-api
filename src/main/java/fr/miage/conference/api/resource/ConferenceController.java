933a93b16c850e151476da16624febd11cadd008
Benjamin VIGNOT <benji.vignot@gmail.com>
2023-03-15 17:51:08 +0100

    feat: add controller and tests (#16)

---
+++ src/main/java/fr/miage/conference/api/resource/ConferenceController.java
@@ -0,0 +1,73 @@
+package fr.miage.conference.api.resource;
+
+import fr.miage.conference.conference.ConferenceService;
+import fr.miage.conference.conference.assembler.ConferenceAssembler;
+import fr.miage.conference.conference.entity.Conference;
+import fr.miage.conference.conference.entity.ConferenceInput;
+import fr.miage.conference.conference.exception.ConferenceNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ejb.EJB;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@RestController
+@RequestMapping(value = "/conferences", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ConferenceController {
+
+    @EJB
+    private ConferenceService service;
+
+    @EJB
+    private ConferenceAssembler assembler;
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    @GetMapping()
+    public List<Conference> getAllConferences() {
+        return service.getAllConferences();
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<EntityModel<Conference>> getConference(@PathVariable("id") String id) throws ConferenceNotFoundException {
+        return Optional.of(service.getConference(id))
+                .map(i -> ResponseEntity.ok(assembler.toModel(i)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping()
+    @Transactional
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<EntityModel<Conference>> createConference(@RequestBody @Valid ConferenceInput conference) {
+        Conference saved = service.createConference(modelMapper.map(conference, Conference.class));
+        URI location = linkTo(ConferenceController.class).slash(saved.getId()).toUri();
+        return ResponseEntity.created(location).body(assembler.toModel(saved));
+    }
+
+    @PatchMapping(value = "/{id}")
+    @Transactional
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<EntityModel<Conference>> updateConference(@PathVariable("id") String id, @RequestBody ConferenceInput conference) throws ConferenceNotFoundException {
+        Conference saved = service.updateConference(id, modelMapper.map(conference, Conference.class));
+        URI location = linkTo(ConferenceController.class).slash(saved.getId()).toUri();
+        return ResponseEntity.ok(assembler.toModel(saved));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @Transactional
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Conference> deleteConference(@PathVariable("id") String id) throws ConferenceNotFoundException {
+        service.deleteConference(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/fr/miage/conference/conference/assembler/ConferenceAssembler.java
@@ -0,0 +1,44 @@
+package fr.miage.conference.conference.assembler;
+
+import fr.miage.conference.api.resource.ConferenceController;
+import fr.miage.conference.conference.entity.Conference;
+import fr.miage.conference.conference.entity.ConferenceInput;
+import lombok.SneakyThrows;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Service;
+
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Singleton
+@Startup
+@Service
+public class ConferenceAssembler implements RepresentationModelAssembler<Conference, EntityModel<Conference>> {
+    @SneakyThrows
+    @Override
+    public EntityModel<Conference> toModel(Conference entity) {
+        return EntityModel.of(entity,
+                linkTo(methodOn(ConferenceController.class).getConference(entity.getId())).withSelfRel(),
+                linkTo(methodOn(ConferenceController.class).updateConference(entity.getId(), new ConferenceInput())).withRel("update"),
+                linkTo(methodOn(ConferenceController.class).deleteConference(entity.getId())).withRel("delete"),
+                linkTo(methodOn(ConferenceController.class).getAllConferences()).withRel("collection"));
+    }
+
+    @Override
+    public CollectionModel<EntityModel<Conference>> toCollectionModel(Iterable<? extends Conference> entities) {
+        List<EntityModel<Conference>> intervenantModel = StreamSupport
+                .stream(entities.spliterator(), false)
+                .map(this::toModel)
+                .toList();
+        return CollectionModel.of(intervenantModel,
+                linkTo(methodOn(ConferenceController.class)
+                        .getAllConferences()).withSelfRel());
+    }
+}
