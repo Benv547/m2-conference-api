bf488296131914f457c0f37d516b35c04363ddc8
Benjamin VIGNOT <benji.vignot@gmail.com>
2023-03-16 15:31:39 +0100

    feat: add controller (#17)

---
+++ src/main/java/fr/miage/conference/api/dto/SessionInput.java
@@ -0,0 +1,18 @@
+package fr.miage.conference.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SessionInput {
+
+    private float prix;
+    private Date date;
+    private String lieu;
+
+}
