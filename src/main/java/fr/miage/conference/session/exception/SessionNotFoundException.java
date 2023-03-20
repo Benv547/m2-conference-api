8e9c7c78541d12e7699ae438badd8d56ef50e874
Benjamin VIGNOT <benji.vignot@gmail.com>
2023-03-20 14:11:20 +0100

    fix: add update function and throws errors (#18)

---
+++ src/main/java/fr/miage/conference/session/exception/SessionNotFoundException.java
@@ -0,0 +1,9 @@
+package fr.miage.conference.session.exception;
+
+public class SessionNotFoundException extends Exception {
+
+    public SessionNotFoundException(String message) {
+        super(message);
+    }
+
+}
