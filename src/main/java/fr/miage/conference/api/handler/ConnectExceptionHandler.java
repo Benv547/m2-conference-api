a0d0a16d8fcad48030741f7a4567df638b2668a1
Benjamin VIGNOT <benji.vignot@gmail.com>
2023-03-22 23:08:15 +0100

    feat: retry connection to DB if down (#31)

---
+++ src/main/java/fr/miage/conference/api/handler/ConnectExceptionHandler.java
@@ -0,0 +1,18 @@
+package fr.miage.conference.api.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
+import java.net.ConnectException;
+
+@RestControllerAdvice
+public class ConnectExceptionHandler {
+
+    @ExceptionHandler(ConnectException.class)
+    public ResponseEntity<Object> handleConnectExceptionHandler(ConnectException e) {
+        return ResponseEntity.status(503).build();
+    }
+
+}
